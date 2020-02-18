eff52f6e00699ad8e676771a9173ed71860c5af9
Jonnyy <caijishiwo@gmail.com>
2020-02-18 15:01:48 -0500

    data

---
+++ src/data.js
@@ -0,0 +1,105 @@
+
+
+export default function() {
+    // simulates data coming from a database.
+    return Promise.resolve(
+      [
+          {
+            id: 1,
+            name: "qwe",
+            amt: 120,
+            transactionDt: "05-01"
+          },
+          {
+            id: 1,
+            name: "qwe",
+            amt: 75,
+            transactionDt: "05-21"
+          },
+          {
+            id: 1,
+            name: "qwe",
+            amt: 94,
+            transactionDt: "05-21"
+          },
+          {
+            id: 1,
+            name: "qwe",
+            amt: 10,
+            transactionDt: "06-01"
+          },
+          {
+            id: 1,
+            name: "qwe",
+            amt: 75,
+            transactionDt: "06-21"
+          },
+          {
+            id: 1,
+            name: "qwe",
+            amt: 200,
+            transactionDt: "07-01"
+          },
+          {
+            id: 1,
+            name: "qwe",
+            amt: 1,
+            transactionDt: "07-04"
+          },
+          {
+            id: 1,
+            name: "qwe",
+            amt: 80,
+            transactionDt: "07-03"
+          },
+          {
+            id: 1,
+            name: "qwe",
+            amt: 224,
+            transactionDt: "07-21"
+          },
+          {
+            id: 2,
+            name: "wer",
+            amt: 125,
+            transactionDt: "05-01"
+          },
+          {
+            id: 2,
+            name: "wer",
+            amt: 75,
+            transactionDt: "05-21"
+          },
+          {
+            id: 2,
+            name: "wer",
+            amt: 10,
+            transactionDt: "06-01"
+          },
+          {
+            id: 2,
+            name: "wer",
+            amt: 75,
+            transactionDt: "06-21"
+          },
+          {
+            id: 2,
+            name: "wer",
+            amt: 200,
+            transactionDt: "07-01"
+          },
+          {
+            id: 2,
+            name: "wer",
+            amt: 224,
+            transactionDt: "07-21"
+          },
+          {
+            id: 3,
+            name: "ert",
+            amt: 120,
+            transactionDt: "06-21"
+          }
+      ]
+    );
+  };
